7c1dc157602772338523e6c31296638b396fc99d
Victor Nicollet <victor-github@nicollet.net>
2011-11-01 18:10:02 +0100

    Adding elementary dispatchers

--- src/api_parse.ml
+++ src/api_parse.ml
@@ -2,6 +2,34 @@
 
 open Lwt
 
+let error text = 
+  return (500, `Object [ "error", `String text ])
+
+let dispatch_root ~args ~more = 
+  error "Not implemented"
+
+let dispatch_all ~args ~more = 
+  error "Not implemented"
+
+let dispatch_db db ~args ~more = 
+  error "Not implemented"
+
+let dispatch_db_all db ~args ~more = 
+  error "Not implemented"
+
+let dispatch_db_store db store ~args ~more = 
+  error "Not implemented"
+ 
+let dispatch_db_store_id db store id ~args ~more = 
+  error "Not implemented"
+
 let parse ~uri ~args ~more = 
-  let _ = Util.uri_explode uri in
-  return (500,`Null)
+  let uri_segments = Util.uri_explode uri in
+  match uri_segments with 
+    | []                  -> dispatch_root                    ~args ~more 
+    | [ "_all" ]          -> dispatch_all                     ~args ~more
+    | [ db ]              -> dispatch_db          db          ~args ~more
+    | [ db ; "_all"]      -> dispatch_db_all      db          ~args ~more
+    | [ db ; store ]      -> dispatch_db_store    db store    ~args ~more
+    | [ db ; store ; id ] -> dispatch_db_store_id db store id ~args ~more
+    | _                   -> error "Invalid URI"
