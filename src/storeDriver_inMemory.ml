fb6ac48c33e373f32566cd4796e4bcb4ff781ec4
Victor Nicollet <victor-github@nicollet.net>
2011-11-02 12:50:20 +0100

    Keep track of changes

--- src/storeDriver_inMemory.ml
+++ src/storeDriver_inMemory.ml
@@ -5,20 +5,36 @@ open BatPervasives
 
 type id = string
 
+open DualMap
+
 (* Storing all available tables ----------------------------------- *)
 
-module StringMap = Map.Make(struct
+module StringT = struct
   type t = id
   let compare = compare
-end)
+end
+
+module StringMap = Map.Make(StringT)
 
 module StringTable = Lock.MapLock(StringMap)
 
+module ChangesMap = DualMap.Make(StringT)(StringT)
+
 type table = {
-  data : Json.t StringTable.t
+  data : Json.t StringTable.t ;
+  changes : ChangesMap.t Lock.t
 } 
 
 let tables = StringTable.create ()
+
+let make_table () = { 
+  data = StringTable.create () ;
+  changes = Lock.create ChangesMap.empty
+}
+
+let tag_change table id = 
+  let cid = Util.fresh () in
+  Lock.update (fun c -> ChangesMap.add c cid id) table.changes
 
 (* Implementing the interface ------------------------------------- *)
 
@@ -32,16 +48,20 @@ let get tid id =
 let delete tid id = 
   let open StringTable.Sugar in 
       match tables.[tid] with None -> return () | Some table ->
-	return (table.data.[id] <- None)
+	match table.data.[id] with 
+	  | None   -> return () (* Nothing to delete *)
+	  | Some _ -> tag_change table id ; table.data.[id] <- None ; return () 
 
 let set tid id json = 
   let open StringTable.Sugar in
       Lock.update (fun tables -> 
 	try let table = StringMap.find tid tables in
-	    table.data.[id] <- Some json ;
-	    tables
+	    match table.data.[id] with
+	      | Some json' when json = json' -> tables (* Nothing to change *)
+	      | _ -> tag_change table id ; table.data.[id] <- Some json ; tables
 	with Not_found ->
-	  let table = { data = StringTable.create () } in
+	  let table = make_table () in
+	  tag_change table id ;
 	  table.data.[id] <- Some json ;
 	  StringMap.add tid table tables
       ) tables ;
