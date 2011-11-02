11aaeac87f94bfa92e01c909d470608714061ec0
Victor Nicollet <victor-github@nicollet.net>
2011-11-02 10:02:25 +0100

    Adding bindings for node-item functionality

--- src/api_parse.ml
+++ src/api_parse.ml
@@ -2,13 +2,16 @@
 
 open Lwt
 
-let error text = 
-  return (500, `Object [ "error", `String text ])
+let fail code text =
+  return (code, `Object [ "error", `String text ])
+
+let error  text = fail 500 text
+let unsupported = fail 405 "Unsupported HTTP method" 
 
 let get_only more f a = 
   match more with 
     | `GET -> f a
-    | _    -> error "Unsupported HTTP method"
+    | _    -> unsupported
 
 let dispatch_root ~args ~more = 
   get_only more Core.status ()
@@ -21,7 +24,7 @@ let dispatch_db db ~args ~more =
     | `GET    -> Core.get_database db
     | `PUT _  -> Core.put_database db
     | `DELETE -> Core.delete_database db
-    | _       -> error "Unsupported HTTP method"
+    | _       -> unsupported
 
 let dispatch_db_all db ~args ~more = 
   get_only more Core.all_nodes db
@@ -34,15 +37,19 @@ let dispatch_db_node db no ~args ~more =
     | _        -> error "Not implemented" 
  
 let dispatch_db_node_id db no id ~args ~more = 
-  error "Not implemented"
+  match more with 
+    | `GET     -> Core.get_item db no id
+    | `PUT put -> Core.put_item db no id put
+    | `DELETE  -> Core.delete_item db no id 
+    | _        -> unsupported
 
 let parse ~uri ~args ~more = 
   let uri_segments = Util.uri_explode uri in
   match uri_segments with 
     | []               -> dispatch_root                ~args ~more 
-    | [ "_all" ]       -> dispatch_all                 ~args ~more
+    | [ "!all" ]       -> dispatch_all                 ~args ~more
     | [ db ]           -> dispatch_db         db       ~args ~more
-    | [ db ; "_all"]   -> dispatch_db_all     db       ~args ~more
+    | [ db ; "!all"]   -> dispatch_db_all     db       ~args ~more
     | [ db ; no ]      -> dispatch_db_node    db no    ~args ~more
     | [ db ; no ; id ] -> dispatch_db_node_id db no id ~args ~more
-    | _                -> error "Invalid URI"
+    | _                -> fail 404 "Invalid URI"
